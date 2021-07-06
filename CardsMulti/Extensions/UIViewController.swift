c2354601b0f03c9a46268e40dab76f58448864dd
victor <xyz@xyz.xyz>
2021-07-06 08:52:12 -0400

    Alerts/dialogs on main thread

--- CardsMulti/Extensions/UIViewController.swift
+++ CardsMulti/Extensions/UIViewController.swift
@@ -26,21 +26,21 @@ extension UIViewController {
      Displays an alert with a text entry and OK and Cancel buttons
      */
     func showTextDialog(title: String, text: String, keyboardType: UIKeyboardType, okAction: @escaping ((String) -> Void)) {
-        let textInputAlert = UIAlertController(title: title, message: text, preferredStyle: .alert)
-        textInputAlert.addTextField()
-        textInputAlert.textFields![0].keyboardType = keyboardType
-        
-        let okButton = UIAlertAction(title: "ok".localized, style: .default) { (alert) -> Void in
-            if let inputText = textInputAlert.textFields![0].text {
-                okAction(inputText)
-            }
-        }
-        let cancelButton = UIAlertAction(title: "cancel".localized, style: .cancel) { (alert) -> Void in }
-        
-        textInputAlert.addAction(okButton)
-        textInputAlert.addAction(cancelButton)
-        
         DispatchQueue.main.async {
+            let textInputAlert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+            textInputAlert.addTextField()
+            textInputAlert.textFields![0].keyboardType = keyboardType
+            
+            let okButton = UIAlertAction(title: "ok".localized, style: .default) { (alert) -> Void in
+                if let inputText = textInputAlert.textFields![0].text {
+                    okAction(inputText)
+                }
+            }
+            let cancelButton = UIAlertAction(title: "cancel".localized, style: .cancel) { (alert) -> Void in }
+            
+            textInputAlert.addAction(okButton)
+            textInputAlert.addAction(cancelButton)        
+        
             self.present(textInputAlert, animated: true, completion: nil)
         }
     }
@@ -49,22 +49,22 @@ extension UIViewController {
      Displays an alert with OK and Cancel buttons
      */
     func showActionDialog(title: String?, text: String?, actionTitle: String, action: @escaping (() -> Void), cancelAction: (() -> Void)? = nil) {
-        let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
-        
-        let action = UIAlertAction(title: actionTitle, style: .default) { (alert) -> Void in
-            action()
-        }
-
-        let cancelAction = UIAlertAction(title: "cancel".localized, style: .cancel) { (alert) -> Void in
-            if cancelAction != nil {
-                cancelAction!()
-            }
-        }
-        
-        alert.addAction(action)
-        alert.addAction(cancelAction)
-        
         DispatchQueue.main.async {
+            let alert = UIAlertController(title: title, message: text, preferredStyle: .alert)
+            
+            let action = UIAlertAction(title: actionTitle, style: .default) { (alert) -> Void in
+                action()
+            }
+
+            let cancelAction = UIAlertAction(title: "cancel".localized, style: .cancel) { (alert) -> Void in
+                if cancelAction != nil {
+                    cancelAction!()
+                }
+            }
+            
+            alert.addAction(action)
+            alert.addAction(cancelAction)
+            
             self.present(alert, animated: true, completion: nil)
         }
     }
