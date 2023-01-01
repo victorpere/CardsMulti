5464e0be01adcec6d72425e48f96a891da989512
victor <victor.perelman@icloud.com>
2023-01-01 15:08:56 -0500

    card update

--- CardsMulti/classes/Card.swift
+++ CardsMulti/classes/Card.swift
@@ -7,8 +7,8 @@
 //
 
 class Card {
-    var suit: Suit
-    var rank: Rank
+    let suit: Suit
+    let rank: Rank
     
     var spriteName: String {
         get {
@@ -25,9 +25,9 @@ class Card {
         self.rank = rank
     }
     
-    func beats(_ card: Card, trump: Card) -> Bool {
+    func beats(_ card: Card, trump: Suit) -> Bool {
         if (self.rank.rawValue > card.rank.rawValue && self.suit == card.suit) ||
-            (self.suit == trump.suit && card.suit != trump.suit) {
+            (self.suit == trump && card.suit != trump) {
             return true
         }
         return false
@@ -39,5 +39,32 @@ class Card {
             return true
         }
         return false
+    }
+    
+    func isNextRank(_ direction: UpOrDown, of adjacentSuitRule: SuitRule, to toCard: Card) -> Bool {
+        if !adjacentSuitRule.checkRule(card1: self, card2: toCard) {
+            return false
+        }
+        
+        return true
+    }
+    
+    enum UpOrDown {
+        case up, down
+    }
+    
+    enum SuitRule {
+        case sameSuit, differentColour, any
+        
+        func checkRule(card1: Card, card2: Card) -> Bool {
+            switch self {
+            case .sameSuit:
+                return card1.suit == card2.suit
+            case .differentColour:
+                return card1.suit.color != card2.suit.color
+            case .any:
+                return true
+            }
+        }
     }
 }
