42b8adfff7d0a6d386a5a190ffd5a8578468a4d3
victor <victor.perelman@icloud.com>
2023-10-28 21:46:51 -0400

    use userDefaults object

--- CardsMulti/StoredBase.swift
+++ CardsMulti/StoredBase.swift
@@ -12,14 +12,14 @@ class StoredBase : NSObject {
     let userDefaults = UserDefaults.standard
     
     func setting<T>(forKey key: String) -> T? {
-        if let value = self.userDefaults.value(forKey: key) as? T {
+        if let value = self.userDefaults.object(forKey: key) as? T {
             return value
         }
         return nil
     }
     
     func settingOrDefault<T>(forKey key: String, defaultValue: T) -> T {
-        if let value = self.userDefaults.value(forKey: key) as? T {
+        if let value = self.userDefaults.object(forKey: key) as? T {
             return value
         }
         self.setSetting(forKey: key, toValue: defaultValue)
@@ -28,7 +28,7 @@ class StoredBase : NSObject {
     
     func setSetting<T>(forKey key: String, toValue value: T?) {
         guard let value = value else { return }
-        self.userDefaults.setValue(value, forKey: key)
+        self.userDefaults.set(value, forKey: key)
     }
     
     func removeSetting(forKey key: String) {
