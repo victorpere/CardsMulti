ced5cd68e279758a49df6d2b1ba4b7d0834b48af
victor <victor.perelman@icloud.com>
2023-11-14 21:30:15 -0500

    card deck update and test

--- CardsMulti/CardDeck.swift
+++ CardsMulti/CardDeck.swift
@@ -8,10 +8,32 @@
 
 import Foundation
 
+/// Represents a deck of cards
 struct CardDeck: Codable {
-    let name: String
+    
+    /// Array of cards in the deck
     let cards: [Card]
     
+    // MARK: - Initializers
+    
+    /// Initialize with an array of cards
+    private init(cards: [Card]) {
+        self.cards = cards
+    }
+    
+    /// Initialize by pack type
+    init(pack: Pack) {
+        switch pack {
+        case .standard:
+            self = CardDeck.standard
+        case .piquet:
+            self = CardDeck.piquet
+        }
+    }
+    
+    // MARK: - Static methods
+    
+    /// Create a standard 52-card deck
     static var standard: CardDeck {
         var cards = [Card]()
         
@@ -21,6 +43,25 @@ struct CardDeck: Codable {
             }
         }
         
-        return CardDeck(name: "standard", cards: cards)
+        return CardDeck(cards: cards)
+    }
+    
+    /// Create a 32-card piquet pack
+    static var piquet: CardDeck {
+        var cards = [Card]()
+        
+        for rank in Rank.allCases.filter({ rank in return rank.rawValue >= 7 }) {
+            for suit in Suit.allCases {
+                cards.append(Card(suit: suit, rank: rank))
+            }
+        }
+        
+        return CardDeck(cards: cards)
+    }
+    
+    /// Pack types
+    enum Pack: String {
+        case standard
+        case piquet
     }
 }
--- CardsMultiTests/GameConfigsTests.swift
+++ CardsMultiTests/GameConfigsTests.swift
@@ -17,4 +17,18 @@ class GameConfigsTests : XCTestCase {
         XCTAssertNotNil(gameConfig)
     }
     
+    func testCardDeck() {
+        let deck = CardDeck.standard
+        let jsonEncoder = JSONEncoder()
+        
+        let jsonData = try? jsonEncoder.encode(deck)
+        XCTAssertNotNil(jsonData)
+        
+        let jsonString = String(data: jsonData!, encoding: .utf8)
+        XCTAssertNotNil(jsonString)
+        
+        print(jsonString!)
+        
+        XCTAssertEqual("{\"cards\":[{\"rank\":2,\"suit\":0},{\"rank\":2,\"suit\":1},{\"rank\":2,\"suit\":2},{\"rank\":2,\"suit\":3},{\"rank\":3,\"suit\":0},{\"rank\":3,\"suit\":1},{\"rank\":3,\"suit\":2},{\"rank\":3,\"suit\":3},{\"rank\":4,\"suit\":0},{\"rank\":4,\"suit\":1},{\"rank\":4,\"suit\":2},{\"rank\":4,\"suit\":3},{\"rank\":5,\"suit\":0},{\"rank\":5,\"suit\":1},{\"rank\":5,\"suit\":2},{\"rank\":5,\"suit\":3},{\"rank\":6,\"suit\":0},{\"rank\":6,\"suit\":1},{\"rank\":6,\"suit\":2},{\"rank\":6,\"suit\":3},{\"rank\":7,\"suit\":0},{\"rank\":7,\"suit\":1},{\"rank\":7,\"suit\":2},{\"rank\":7,\"suit\":3},{\"rank\":8,\"suit\":0},{\"rank\":8,\"suit\":1},{\"rank\":8,\"suit\":2},{\"rank\":8,\"suit\":3},{\"rank\":9,\"suit\":0},{\"rank\":9,\"suit\":1},{\"rank\":9,\"suit\":2},{\"rank\":9,\"suit\":3},{\"rank\":10,\"suit\":0},{\"rank\":10,\"suit\":1},{\"rank\":10,\"suit\":2},{\"rank\":10,\"suit\":3},{\"rank\":11,\"suit\":0},{\"rank\":11,\"suit\":1},{\"rank\":11,\"suit\":2},{\"rank\":11,\"suit\":3},{\"rank\":12,\"suit\":0},{\"rank\":12,\"suit\":1},{\"rank\":12,\"suit\":2},{\"rank\":12,\"suit\":3},{\"rank\":13,\"suit\":0},{\"rank\":13,\"suit\":1},{\"rank\":13,\"suit\":2},{\"rank\":13,\"suit\":3},{\"rank\":14,\"suit\":0},{\"rank\":14,\"suit\":1},{\"rank\":14,\"suit\":2},{\"rank\":14,\"suit\":3}]}", jsonString)
+    }
 }
