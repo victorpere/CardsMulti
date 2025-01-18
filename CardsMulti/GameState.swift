4c7827525eeba35946a538ae8b0d2c267e54c8ca
victor <victor.perelman@icloud.com>
2025-01-18 16:12:41 -0500

    save previous states

--- CardsMulti/GameState.swift
+++ CardsMulti/GameState.swift
@@ -17,9 +17,13 @@ class GameState {
     // MARK: - Properties
     
     private let gameType: GameType?
+    private var previousSceneStates: [SceneState] = []
     
-    @StoredEncodedWithDefault var cardNodes: [CardSpriteNode]
-    @StoredEncodedWithDefault var scores: [Score]
+    @StoredEncodedWithDefault var sceneState: SceneState
+    
+    
+//    @StoredEncodedWithDefault var cardNodes: [CardSpriteNode]
+//    @StoredEncodedWithDefault var scores: [Score]
     @StoredValue (key: "gameId") var gameId: String?
     
     // MARK: - Computed properties
@@ -34,16 +38,42 @@ class GameState {
     // MARK: - Initializers
     
     init() {
+        @StoredEncodedWithDefault var cardNodes: [CardSpriteNode]
         _cardNodes = StoredEncodedWithDefault(key: "_cardNodes", defaultValue: [])
+        @StoredEncodedWithDefault var scores: [Score]
         _scores = StoredEncodedWithDefault(key: "_scores", defaultValue: [])
 
         self.gameType = nil
+        
+        let sceneState = SceneState(cardNodes: cardNodes, scores: scores)
+        _sceneState = StoredEncodedWithDefault(key: "_sceneState", defaultValue: sceneState)
     }
     
     init(_ gameType: GameType) {
+        @StoredEncodedWithDefault var cardNodes: [CardSpriteNode]
         _cardNodes = StoredEncodedWithDefault(key: "\(gameType.rawValue)_cardNodes", defaultValue: [])
+        @StoredEncodedWithDefault var scores: [Score]
         _scores = StoredEncodedWithDefault(key: "\(gameType.rawValue)_scores", defaultValue: [])
         
         self.gameType = gameType
+        
+        let sceneState = SceneState(cardNodes: cardNodes, scores: scores)
+        _sceneState = StoredEncodedWithDefault(key: "\(gameType.rawValue)_sceneState", defaultValue: sceneState)
+    }
+    
+    // MARK: - Public methods
+    
+    func save(cardNodes: [CardSpriteNode], scores: [Score]) {
+        let previousSceneState = self.sceneState
+        
+        self.previousSceneStates.append(previousSceneState)
+        
+        self.sceneState = SceneState(cardNodes: cardNodes, scores: scores)
+    }
+    
+    func undo() {
+        if self.previousSceneStates.count > 0, let previousSceneState = self.previousSceneStates.popLast() {
+            self.sceneState = previousSceneState
+        }
     }
 }
